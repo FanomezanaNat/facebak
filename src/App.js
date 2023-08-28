481f79ed3a2f4e739ba7526086180e1a4b8abb43
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-08-28 17:11:43 +0300

    fix: add SignUp import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './assets/css/App.css';
 import Navbar from './layout/Navbar';
 import './assets/css/Navbar.css'
+import SignUp from './layout/pages/Sign_up/SignUp.jsx';
+
 function App() {
   return (
     <>
